861c8988e5ac098aa3cfaf28fedf6e7d0657eda3
Body560 <body809070@gmail.com>
2023-06-16 11:03:07 -0700

    d

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("value is: %d bytes\n", sizeof(int));
-printf("value is: %d bytes\n", sizeof(char));
-printf("value is: %d bytes\n", sizeof(float));
-printf("value is: %d bytes\n", sizeof(long));
-printf("value is: %d bytes\n", sizeof(long int));
+printf("Size of a char: %d bytes(s)\n", sizeof(char));
+printf("Size of a int : %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", sizeof(float));
 return (0);
 }
