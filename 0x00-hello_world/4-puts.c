99afcf0e02927b4b62af5d32e64a771311f890a2
Body560 <body809070@gmail.com>
2023-06-16 09:26:50 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.hd>
+/**
+* main - this main function
+*
+*Return: return value 0
+*/
+int main(void)
+{
+        puts("Programming is like building a multilingual puzz\n");
+        return (0);
+}
+
