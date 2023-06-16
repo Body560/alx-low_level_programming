52381b0b14def55668059e8feb7d2a1fc178934a
Body560 <body809070@gmail.com>
2023-06-16 10:57:41 -0700

    s

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - function main
+ *
+ *Return: return 0
+ */
+int main(void)
+{
+printf("value is: %d bytes\n", sizeof(int));
+printf("value is: %d bytes\n", sizeof(char));
+printf("value is: %d bytes\n", sizeof(float));
+printf("value is: %d bytes\n", sizeof(long));
+printf("value is: %d bytes\n", sizeof(long int));
+return (0);
+}
