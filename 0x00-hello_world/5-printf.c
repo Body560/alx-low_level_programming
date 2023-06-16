2eaf8527dc3092aaf7cb2485936e0bf7f80a2e3a
Body560 <body809070@gmail.com>
2023-06-16 10:15:04 -0700

    sd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main - main fun
+ *main- main fun
  *
- *Return: well Return 0(Success) 
+ *Return: well return
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
