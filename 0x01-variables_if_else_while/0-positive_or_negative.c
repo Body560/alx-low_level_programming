5bf3817adfbbc141d6b98d0a2e1caaaf4d28666d
Body560 <body809070@gmail.com>
2023-06-17 16:32:01 -0700

    1&2

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - main if function
+ * @max - this fun will be return random number
+ * Return: will be return 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is nagative\n", n);
+	}
+	return (0);
+}
